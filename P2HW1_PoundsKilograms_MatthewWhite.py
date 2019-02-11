202b89deb61b406ca178c69531f23a6e3288bcb3
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2019-02-11 11:29:18 -0500

    CTI 110 PoundsKilograms

---
+++ P2HW1_PoundsKilograms_MatthewWhite.py
@@ -0,0 +1,25 @@
+''' Pounds to kilograms converter
+    CTI-110 P2HW1 - Pounds to kilograms converter
+    02/11/2019
+    Matthew White '''
+
+
+# Pounds to kilogram converter
+
+
+
+
+# Ask user for weight in pounds
+
+pounds = int(input("Enter the weight in pounds: "))
+
+
+# Convert Pounds to kilograms
+
+kilograms = pounds / 2.2046
+
+
+#Display the result
+
+print (pounds, "pounds are converted to", format(kilograms,'.2f'), "kilograms")
+    
