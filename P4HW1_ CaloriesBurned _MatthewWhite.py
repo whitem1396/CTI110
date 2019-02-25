d763124e0bb5bba587a63d3987bd3cb8677dd349
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2019-02-25 12:14:04 -0500

    P4HW1 Calorie

---
+++ P4HW1_ CaloriesBurned _MatthewWhite.py
@@ -0,0 +1,19 @@
+
+
+
+
+print ('Age\tDog years')
+print ('----------------')
+
+for age in [10,20,30]:
+    
+    dog_age = 7 * age
+    
+    print  (age,"\t",dog_age)
+    
+    
+    
+    
+    
+    
+
